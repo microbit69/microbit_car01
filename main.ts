93873c8d102d54796b6fefb06eab3b0245c22c59
microbit69 <106347913+microbit69@users.noreply.github.com>
2022-05-28 00:10:39 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
+})
 basic.forever(function () {
 	
 })

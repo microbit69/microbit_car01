37bdbd14b0df99fa3d2cc5c5386b8757021f2db8
microbit69 <106347913+microbit69@users.noreply.github.com>
2022-05-28 20:02:08 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P1, 180)
-    pins.servoWritePin(AnalogPin.P2, 180)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    set_Servo_backwards(1)
 })
 input.onButtonPressed(Button.AB, function () {
-    pins.servoWritePin(AnalogPin.P1, 86)
-    pins.servoWritePin(AnalogPin.P2, 86)
+    pins.servoWritePin(AnalogPin.P1, servo_stop)
+    pins.servoWritePin(AnalogPin.P2, servo_stop)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P1, 0)
-    pins.servoWritePin(AnalogPin.P2, 0)
+    pins.servoWritePin(AnalogPin.P1, servo_stop)
+    pins.servoWritePin(AnalogPin.P2, servo_stop)
 })
+function set_Servo_backwards (servo_speed: number) {
+    // Rechts in Fahrtrichtung
+    pins.servoWritePin(AnalogPin.P1, 75)
+    pins.servoWritePin(AnalogPin.P2, 120)
+}
+let servo_stop = 0
+servo_stop = 85
+let servo_speed = 0
 basic.forever(function () {
 	
 })

33d9b6376bdca8829edd1446b5b074d5a54378b3
microbit69 <106347913+microbit69@users.noreply.github.com>
2022-05-30 23:16:13 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,35 @@
+function set_Servo_forward (distance_cm: number) {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . . . . .
+        . . . . .
+        `)
+    // Rechts in Fahrtrichtung
+    pins.servoWritePin(AnalogPin.P1, 0)
+    // Links in Fahrtrichtung
+    pins.servoWritePin(AnalogPin.P2, 180)
+    control.waitMicros(57000 * distance_cm)
+    pins.servoWritePin(AnalogPin.P1, servo_stop)
+    pins.servoWritePin(AnalogPin.P2, servo_stop)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # . . . .
+        . # . . .
+        . . # . .
+        `)
+    // Links in Fahrtrichtung
+    pins.servoWritePin(AnalogPin.P2, 180)
+    control.waitMicros(57000 * distance_cm)
+    pins.servoWritePin(AnalogPin.P2, servo_stop)
+}
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(3)
     basic.showNumber(2)
     basic.showNumber(1)
-    set_Servo_backwards(1)
+    set_Servo_forward(10)
 })
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P1, servo_stop)
@@ -12,15 +39,8 @@ input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P1, servo_stop)
     pins.servoWritePin(AnalogPin.P2, servo_stop)
 })
-function set_Servo_backwards (servo_speed: number) {
-    basic.showIcon(IconNames.No)
-    // Rechts in Fahrtrichtung
-    pins.servoWritePin(AnalogPin.P1, 0)
-    // Links in Fahrtrichtung
-    pins.servoWritePin(AnalogPin.P2, 180)
-}
 let servo_stop = 0
-servo_stop = 90
+servo_stop = 89
 let servo_speed = 0
 basic.forever(function () {
 	
